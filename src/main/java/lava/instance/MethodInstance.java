9242b190699a1376e52e7215af6bd84847051711
uhealin <uhealin@nate.com>
2018-04-07 23:48:37 +0800

    代码优化

--- src/main/java/lava/instance/MethodInstance.java
+++ src/main/java/lava/instance/MethodInstance.java
@@ -1,9 +1,18 @@
 package lava.instance;
 
+import java.lang.reflect.InvocationTargetException;
+
+
 public enum MethodInstance {
 
 	
 	close;
 	
 	
+	public void invoke(Object...objects) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+	
+		for(Object object:objects) {
+			object.getClass().getMethod(this.name()).invoke(object);
+		}
+	}
 }

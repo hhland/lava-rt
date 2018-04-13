7d8696671c3f668229199f5451b6cacc62a0365b
uhealin <uhealin@nate.com>
2018-04-13 16:59:55 +0800

    去除中文乱码

--- src/main/java/lava/rt/common/IOCommon.java
+++ src/main/java/lava/rt/common/IOCommon.java
@@ -21,22 +21,22 @@ public class IOCommon {
 	    try {
 	      String urlNameString = url + "?" + param;
 	      URL realUrl = new URL(urlNameString);
-	      // �򿪺�URL֮�������
+	      
 	      URLConnection connection = realUrl.openConnection();
-	      // ����ͨ�õ���������
+	  
 	      connection.setRequestProperty("accept", "*/*");
 	      connection.setRequestProperty("connection", "Keep-Alive");
 	      connection.setRequestProperty("user-agent",
 	          "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-	      // ����ʵ�ʵ�����
+	      
 	      connection.connect();
-	      // ��ȡ������Ӧͷ�ֶ�
+	     
 	      Map<String, List<String>> map = connection.getHeaderFields();
-	      // �������е���Ӧͷ�ֶ�
+	      
 	      for (String key : map.keySet()) {
 	        System.out.println(key + "--->" + map.get(key));
 	      }
-	      // ���� BufferedReader����������ȡURL����Ӧ
+	      
 	      in = new BufferedReader(new InputStreamReader(
 	          connection.getInputStream()));
 	      String line;
@@ -44,10 +44,10 @@ public class IOCommon {
 	        result += line; 
 	      }
 	    } catch (Exception e) {
-	      System.out.println("����GET��������쳣��" + e);
+	      
 	      e.printStackTrace();
 	    }
-	    // ʹ��finally�����ر�������
+	  
 	    finally {
 	      try {
 	        if (in != null) {
@@ -60,34 +60,26 @@ public class IOCommon {
 	    return result;
 	  }
 	 
-	  /**
-	   * ��ָ�� URL ����POST����������
-	   * 
-	   * @param url
-	   *      ��������� URL
-	   * @param param
-	   *      ����������������Ӧ���� name1=value1&name2=value2 ����ʽ��
-	   * @return ������Զ����Դ����Ӧ���
-	   */
+	 
 	  public static String post(String url, Map<String, Object> paramMap) {
 	    PrintWriter out = null;
 	    BufferedReader in = null;
 	    String result = "";
 	    try {
 	      URL realUrl = new URL(url);
-	      // �򿪺�URL֮�������
+	    
 	      URLConnection conn = realUrl.openConnection();
-	      // ����ͨ�õ���������
+	     
 	      conn.setRequestProperty("accept", "*/*");
 	      conn.setRequestProperty("connection", "Keep-Alive");
 	      conn.setRequestProperty("user-agent",
 	          "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-	      // ����POST�������������������
+	      
 	      conn.setDoOutput(true);
 	      conn.setDoInput(true);
 	     
 	      out = new PrintWriter(conn.getOutputStream());
-	      // �����������
+	      
 	      String param="";
 	      
 	      for(Iterator<String> it=paramMap.keySet().iterator();it.hasNext();) {
@@ -98,9 +90,9 @@ public class IOCommon {
 	      }
 	      
 	      out.print(param);
-	      // flush������Ļ���
+	      
 	      out.flush();
-	      // ����BufferedReader����������ȡURL����Ӧ
+	     
 	      in = new BufferedReader(
 	          new InputStreamReader(conn.getInputStream()));
 	      String line;
@@ -108,10 +100,10 @@ public class IOCommon {
 	        result += line;
 	      }
 	    } catch (Exception e) {
-	      System.out.println("���� POST ��������쳣��"+e);
+	     
 	      e.printStackTrace();
 	    }
-	    //ʹ��finally�����ر��������������
+	   
 	    finally{
 	      try{
 	        if(out!=null){
